cd897d43b006663dab9830bb564f88c46b557bd8
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-09-15 20:00:00 -0300

    Finalizando com as validações.

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,111 @@
 import "./App.css";
 
-import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+type FormData = {
+    name: string;
+    date: string;
+    subject: string;
+    description: string;
+};
+
+const schema = yup.object({
+    name: yup.string().required("O nome é obrigatório"),
+    date: yup.string().required("A data é obrigatória"),
+    subject: yup.string().required("O assunto é obrigatório"),
+    description: yup
+        .string()
+        .required("A descrição é obrigatória")
+        .min(10, "No mínimo 10 caracteres"),
+});
 
 export default function App() {
-    const [name, setName] = useState("Controlando");
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormData>({
+        defaultValues: {
+            name: "",
+            date: "",
+            subject: "",
+            description: "",
+        },
+        resolver: yupResolver(schema),
+    });
 
-    function onSubmit(event: React.FormEvent<HTMLElement>) {
-        event.preventDefault();
-
-        setName("");
+    function onSubmit(data: FormData) {
+        console.log(data);
     }
+
     return (
         <div>
             <h1>Evento</h1>
 
-            <form onSubmit={onSubmit}>
-                <input
-                    type="text"
-                    placeholder="Nome do evento"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Controller
+                    control={control}
+                    name="name"
+                    render={({ field }) => (
+                        <input
+                            type="text"
+                            placeholder="Nome do evento"
+                            {...field}
+                        />
+                    )}
                 />
-                <span className="error">Nome é obrigatório</span>
 
-                <input type="date" placeholder="Nome do evento" lang="pt-BR" />
+                {errors.name?.message && (
+                    <span className="error">{errors.name.message}</span>
+                )}
 
-                <select defaultValue="">
-                    <option value="" disabled>
-                        Selecione...
-                    </option>
+                <Controller
+                    control={control}
+                    name="date"
+                    render={({ field }) => (
+                        <input
+                            type="date"
+                            placeholder="Nome do evento"
+                            {...field}
+                        />
+                    )}
+                />
 
-                    <option value="technology">React</option>
-                    <option value="entertainment">Node.js</option>
-                    <option value="business">Javascript</option>
-                    <option value="business">Typescript</option>
-                </select>
+                {errors.date?.message && (
+                    <span className="error">{errors.date.message}</span>
+                )}
 
-                <textarea placeholder="Descrição" rows={4} />
+                <Controller
+                    control={control}
+                    name="subject"
+                    render={({ field }) => (
+                        <select {...field}>
+                            <option value="" disabled>
+                                Selecione...
+                            </option>
 
+                            <option value="react">React</option>
+                            <option value="node">Node.js</option>
+                            <option value="javascript">Javascript</option>
+                            <option value="typescript">Typescript</option>
+                        </select>
+                    )}
+                />
+                {errors.subject?.message && (
+                    <span className="error">{errors.subject.message}</span>
+                )}
+                <Controller
+                    control={control}
+                    name="description"
+                    render={({ field }) => (
+                        <textarea {...field} placeholder="Descrição" rows={5} />
+                    )}
+                />
+                {errors.description?.message && (
+                    <span className="error">{errors.description.message}</span>
+                )}
                 <button type="submit">Salvar</button>
             </form>
         </div>
